80f72299fdc09de000fd5442cd6d7cb3446b5dd5
Yohei Kokubo <yhei.register@gmail.com>
2020-10-06 00:13:52 +0900

    Managerを使って書籍一覧にレビュー数を付与する(DDDの第一歩) #1

--- cms/models.py
+++ cms/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 
+class BookManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter()
+    def getWithImpressionCount(self):
+      books = Book.objects.all().order_by('id')
+      for book in books:
+        book.impressionCount = Impression.objects.filter(book_id=book.id).count()
+      return books
+
 class Book(models.Model):
   """書籍"""
+  objects = BookManager()
   name = models.CharField('書籍名', max_length=255)
   publisher = models.CharField('出版社', max_length=255, blank=True)
   page = models.IntegerField('ページ数', blank=True, default=0)
+  impressionCount = 0
 
   def __str__(self):
     return self.name
